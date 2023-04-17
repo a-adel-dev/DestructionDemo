0f4852f0291530c5b4347d52e266bf54dc798ac2
Ahmad Adel Fouad <a.adel.dev@gmail.com>
2023-04-17 10:59:45 +0200

    Demo scene

---
+++ Assets/2D_Destruction/Demo/Demo Scripts/ExplodeOnClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Explodable))]
+public class ExplodeOnClick : MonoBehaviour {
+
+	private Explodable _explodable;
+	private WallSpawner _wallSpawner;
+
+	void Start()
+	{
+		_explodable = GetComponent<Explodable>();
+		_wallSpawner = FindObjectOfType<WallSpawner>();
+	}
+	void OnMouseDown()
+	{
+		_explodable.explode();
+		ExplosionForce ef = FindObjectOfType<ExplosionForce>();
+		ef.doExplosion(transform.position);
+		_wallSpawner.isWallInScene = false;
+	}
+}
---
+++ Assets/2D_Destruction/Demo/Demo Scripts/RemoveFragments.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RemoveFragments : ExplodableAddon
+{
+    [SerializeField] private float fragmentsWaitTime = 2.5f;
+    public override void OnFragmentsGenerated(List<GameObject> fragments)
+    {
+        foreach (var fragment in fragments)
+        {
+            Destroy(fragment,fragmentsWaitTime);
+        }
+    }
+}
